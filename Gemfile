d274e374436c377762a5e2b860ea6058363e063e
bUg <aleks.grebennik@gmail.com>
2010-11-02 03:22:52 +0200

    init

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'http://rubygems.org'
+
+gem 'rails',                 :git => 'git://github.com/rails/rails.git', :branch => '3-0-stable'
+gem 'sqlite3-ruby',          :require => 'sqlite3'
+gem 'devise',                :git => 'git://github.com/plataformatec/devise.git'
+#, :branch => 'omniauth'
+#gem 'omniauth',              :git => 'git://github.com/intridea/omniauth.git'
+
+group :development, :test do
+  gem 'mongrel',             '1.2.0.pre2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sometest::Application.config.secret_token = '79b298cc37db5395e1a53f200c857d247ccc8ef3b6f68107a9f89ceb96a21c5bbb69c770190511b6baa45c7595671c91efcba235b497e50310d2ad03a3e564ba'
